37e831530d681a27a974dbf60464e8cb0e6cd5c7
ayreen2 <ayreen.japutri.2@gmail.com>
2021-05-27 19:26:09 +0200

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -21,6 +21,7 @@ import sys
 
 # Sum of all sales (values) is initialized with zero, we just started
 count_of_values = 0
+count = 0 
 
 # Previous key is initialized with None, we just started
 previous_key = None
@@ -45,17 +46,25 @@ for line in sys.stdin:
         # to the standart output (stdout)
         # Key and value are seperated by a tab (\t)
         # Line ends with new line (\n)
-        sys.stdout.write("{0}\t{1}\n".format(previous_key, count_of_values))
-        # Sum of sales starts again with 0
-        count_of_values = 0
+	    if count > 114: 
+                avg=count_of_values/count
+                sys.stdout.write("{0}\t{1}\n".format(previous_key, avg))
+            # Sum of sales starts again with 0
+                count_of_values = 0
+                count=0	
 
     # Add the value to the total sales
     # a += b is the same as a = a + b
     # the float function transforms the value
     # to a float data type (like decimal)
-    count_of_values = data.count(key)
-    # the previous key for the next iteration is the current key of the this iteration 
+    count += 1
+    count_of_values += float(value)
+    
+    
+   # the previous key for the next iteration is the current key of the this iteration 
     previous_key = key
 
 # write the last result to stdout
-sys.stdout.write("{0}\t{1}\n".format(previous_key, count_of_values))
+if count > 144: 
+	avg=count_of_values/count
+	sys.stdout.write("{0}\t{1}\n".format(previous_key, avg))
